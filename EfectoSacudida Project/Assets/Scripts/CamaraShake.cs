82c0000831298ecf5756557b4ecfeeb9b33485bc
lPinchol <pinchodev91@gmail.com>
2017-02-25 15:54:12 +0100

    Add Script

---
+++ EfectoSacudida Project/Assets/Scripts/CamaraShake.cs
@@ -0,0 +1,23 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// CamaraShake.cs (25/02/2017)													\\
+// Autor: Antonio Mateo (Moon Pincho) 									        \\
+// Descripcion:		Efecto Shake(Sacudida) de la camara							\\
+// Fecha Mod:		25/02/2017													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonPincho
+{
+    /// <summary>
+    /// <para>Efecto Shake(Sacudida) de la camara</para>
+    /// </summary>
+	public class CamaraShake : MonoBehaviour 
+	{
+
+	}
+}
